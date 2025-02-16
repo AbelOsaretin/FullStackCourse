b2097a021e896eb1b39f0241e0f9dc0efed24ef3
Abel Osaretin <contact.abel321@gmail.com>
2025-02-16 20:58:44 +0100

    1.9: unicafe step 4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -18,7 +18,14 @@ const Button = ({ onClick, text }) => {
 
 // calTotal();
 
-const Statistics = ({ headerText, good, neutral, bad }) => {
+const Statistics = ({ headerText, good, neutral, bad, allClicks }) => {
+  if (allClicks.length === 0) {
+    return (
+      <div>
+        <p>No Feedback Given, give feedback first</p>
+      </div>
+    );
+  }
   const totaRating = good + neutral + bad;
   return (
     <div>
@@ -38,18 +45,35 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [allClicks, setAllClicks] = useState([]);
+
+  const handleGoodButtonClick = () => {
+    setAllClicks(allClicks.concat(allClicks.length + 1));
+    setGood(good + 1);
+  };
+
+  const handleNeutralButtonClick = () => {
+    setAllClicks(allClicks.concat(allClicks.length + 1));
+    setNeutral(neutral + 1);
+  };
+
+  const handleBadButtonClick = () => {
+    setAllClicks(allClicks.concat(allClicks.length + 1));
+    setBad(bad + 1);
+  };
 
   return (
     <div>
       <Header text={"give feedback"} />
-      <Button text={"good"} onClick={() => setGood(good + 1)} />
-      <Button text={"neutral"} onClick={() => setNeutral(neutral + 1)} />
-      <Button text={"bad"} onClick={() => setBad(bad + 1)} />
+      <Button text={"good"} onClick={handleGoodButtonClick} />
+      <Button text={"neutral"} onClick={handleNeutralButtonClick} />
+      <Button text={"bad"} onClick={handleBadButtonClick} />
       <Statistics
         headerText={"statistics"}
         good={good}
         neutral={neutral}
         bad={bad}
+        allClicks={allClicks}
       />
     </div>
   );
