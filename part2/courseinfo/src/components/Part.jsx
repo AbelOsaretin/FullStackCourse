989b5899906baf481402a377e89145cbfbb64ee9
Abel Osaretin <contact.abel321@gmail.com>
2025-03-02 18:59:53 +0100

    2.1: Course information step 6

--- part2/courseinfo/src/components/Part.jsx
+++ part2/courseinfo/src/components/Part.jsx
@@ -2,7 +2,10 @@ const Part = ({ part }) => {
   return (
     <ul>
       {part.map((parts) => (
-        <li key={parts.id}>{parts.name}</li>
+        <li key={parts.id}>
+          {parts.name}
+          {parts.exercises}
+        </li>
       ))}
     </ul>
   );
