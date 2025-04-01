39a1e97bc2c3b412a61376e13670873a3ccf64a1
Abel Osaretin <contact.abel321@gmail.com>
2025-04-01 18:18:56 +0100

    2.4: Course information step 9

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -2,13 +2,25 @@ import Header from "./Header";
 import Content from "./Content";
 import Total from "./Total";
 const Course = ({ course }) => {
-  return (
-    <div>
-      <Header name={course.name} />
-      <Content part={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
+  console.log("Courses", course);
+
+  return course.map((courses) => {
+    return (
+      <div key={courses.id}>
+        <Header name={courses.name} />
+        <Content part={courses.parts} />
+        <Total parts={courses.parts} />
+      </div>
+    );
+  });
+
+  // return (
+  //   <div>
+  //     <Header name={course[1].name} />
+  //     <Content part={course[1].parts} />
+  //     <Total parts={course[1].parts} />
+  //   </div>
+  // );
 };
 
 export default Course;
--- part2/courseinfo/src/components/Total.jsx
+++ part2/courseinfo/src/components/Total.jsx
@@ -1,14 +1,8 @@
 const Total = ({ parts }) => {
-  console.log("Parts For Total Cal", parts);
   var result = parts.reduce((sum, part) => {
-    console.log("Current Sum", sum);
-
-    console.log("Current exercises", part.exercises);
-
     return sum + part.exercises;
   }, 0);
 
-  console.log("Total Result", result);
   return <h3>Total of {result} excercises</h3>;
 };
 
