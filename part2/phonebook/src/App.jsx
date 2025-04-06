4e2630fe1b2d6b20ab2c28818f3ef0ca0280d902
Abel Osaretin <contact.abel321@gmail.com>
2025-04-06 01:29:13 +0100

    2.8: The Phonebook Step 3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,9 +3,14 @@ import { useState } from "react";
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleNameInput = (event) => {
     setNewName(event.target.value);
+  };
+
+  const handleNumberInput = (event) => {
+    setNewNumber(event.target.value);
   };
 
   const handleFormSubmit = (event) => {
@@ -13,6 +18,7 @@ const App = () => {
 
     const newPersonObj = {
       name: newName,
+      number: newNumber,
     };
 
     if (!persons.find((person) => person.name == newPersonObj.name)) {
@@ -20,6 +26,7 @@ const App = () => {
     } else {
       alert(`${newName} is already added to phonebook`);
     }
+    console.log(persons);
   };
 
   return (
@@ -30,13 +37,20 @@ const App = () => {
           name: <input value={newName} onChange={handleNameInput} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberInput} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
 
       {persons.map((person, i) => {
-        return <div key={i}>{person.name}</div>;
+        return (
+          <div key={i}>
+            {person.name} {person.number}
+          </div>
+        );
       })}
     </div>
   );
