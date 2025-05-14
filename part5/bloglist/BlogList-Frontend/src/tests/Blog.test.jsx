2fc7409646e0538e6418de757dce144233b70e65
Abel Osaretin <contact.abel321@gmail.com>
2025-05-14 21:14:38 +0100

    5.15: Blog List Tests, step 3

--- part5/bloglist/BlogList-Frontend/src/tests/Blog.test.jsx
+++ part5/bloglist/BlogList-Frontend/src/tests/Blog.test.jsx
@@ -17,6 +17,10 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from '../components/Blog'
+import axios from 'axios'
+
+vi.mock('axios')
+
 
 test('renders title and author, but not url or likes by default', () => {
   const blog = {
@@ -65,3 +69,70 @@ test('clicking the view button displays blog URL and number of likes', async () 
   expect(screen.getByText('http://example.com')).toBeInTheDocument()
   expect(screen.getByText(/likes 5/)).toBeInTheDocument()
 })
+
+// test('clicking the like button twice calls event handler twice', async () => {
+//   const blog = {
+//     title: 'Component testing is done with react-testing-library',
+//     author: 'abel',
+//     url: 'http://example.com',
+//     likes: 5,
+//     user: { name: 'Dev A' },
+//     id: '123'
+//   }
+
+//   const user = { name: 'Dev A' }
+
+//   const mockLikeHandler = vi.fn()
+
+//   // Wrap Blog so it accepts a prop for the like handler
+//   render(
+//     <Blog blog={blog} user={user} handleLike={mockLikeHandler} />
+//   )
+
+//   const userSim = userEvent.setup()
+
+//   // Reveal the like button
+//   const viewButton = screen.getByText('view')
+//   await userSim.click(viewButton)
+
+//   // Like button should now be visible
+//   const likeButton = screen.getByText('like')
+//   await userSim.click(likeButton)
+//   await userSim.click(likeButton)
+
+//   expect(mockLikeHandler).toHaveBeenCalledTimes(2)
+// })
+
+test('clicking the like button twice calls event handler twice', async () => {
+  // Mock the PUT request to prevent real network calls
+  axios.put.mockResolvedValue({
+    data: {
+      likes: 6,
+    }
+  })
+
+  const blog = {
+    title: 'Testing Blog',
+    author: 'Test Author',
+    url: 'http://test.com',
+    likes: 5,
+    user: { name: 'Dev A' },
+  }
+
+  const dbUser = { name: 'Dev A' }
+
+  render(<Blog blog={blog} user={dbUser} />)
+
+  const user = userEvent.setup()
+
+  const viewButton = screen.getByText('view')
+  await user.click(viewButton)
+
+  const likeButton = screen.getByText('like')
+  await user.click(likeButton)
+  await user.click(likeButton)
+
+  // You can also check axios.put was called twice
+  expect(axios.put).toHaveBeenCalledTimes(2)
+})
+
