9e969570088737dd2585929b9e428dce4f107ac4
Abel Osaretin <contact.abel321@gmail.com>
2025-05-14 20:20:15 +0100

    5.14: Blog List Tests, step 2

--- part5/bloglist/BlogList-Frontend/src/tests/Blog.test.jsx
+++ part5/bloglist/BlogList-Frontend/src/tests/Blog.test.jsx
@@ -15,6 +15,7 @@
 
 
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from '../components/Blog'
 
 test('renders title and author, but not url or likes by default', () => {
@@ -42,4 +43,25 @@ test('renders title and author, but not url or likes by default', () => {
 
   const likes = container.querySelector('.blog-title-author')
   expect(likes).not.toHaveTextContent('likes 5')
+})
+
+test('clicking the view button displays blog URL and number of likes', async () => {
+  const blog = {
+    title: 'Component testing is done with react-testing-library',
+    author: 'abel',
+    url: 'http://example.com',
+    likes: 5,
+    user: { name: 'Dev A' }
+  }
+
+  const user = { name: 'Dev A' }
+
+  render(<Blog blog={blog} user={user} />)
+
+  const userSim = userEvent.setup()
+  const viewButton = screen.getByText('view')
+  await userSim.click(viewButton)
+
+  expect(screen.getByText('http://example.com')).toBeInTheDocument()
+  expect(screen.getByText(/likes 5/)).toBeInTheDocument()
 })
