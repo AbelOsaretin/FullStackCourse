b85e3a917c584337c6fcc63034804f2ce66f760b
Abel Osaretin <contact.abel321@gmail.com>
2025-12-21 09:41:49 +0100

    5.18: Blog List End To End Testing, step 2

--- part5/bloglist/Playwright/tests/blog.spec.js
+++ part5/bloglist/Playwright/tests/blog.spec.js
@@ -3,14 +3,29 @@ import { test, expect, describe, beforeEach } from '@playwright/test';
 
 
 describe('Playwright blog homepage', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+
+    // Reset the backend state
+     await request.post('http://localhost:3003/api/testing/reset');
+
+    // Create a user
+     await request.post('http://localhost:3003/api/users', {
+      data: {
+        name: 'Matti Luukkainen',
+        username: 'mluukkai',
+        password: 'salainen'
+      }
+    });
+
+    // Go to app homepage
     await page.goto('http://localhost:5173/');
+
   });
 
 
 test('Login form is shown', async ({ page }) => {
 
-  // Expect a title "to contain" a substring.
+  
   await expect(page.getByRole('heading', { name: 'Log in to application' })).toBeVisible();
 });
 
