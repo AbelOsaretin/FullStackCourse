5e989b811c1f515ccb1a96c9c40be2a0cf63428b
Abel Osaretin <contact.abel321@gmail.com>
2025-12-29 22:35:08 +0100

    Exercises 6.10.-6.13.

--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -1,17 +1,8 @@
 import ReactDOM from "react-dom/client";
-import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 
 import App from "./App";
-import reducer from "./reducers/anecdoteReducer";
-import filterReducer from "./reducers/filterReducer";
-
-const rootReducer = combineReducers({
-  anecdotes: reducer,
-  filter: filterReducer,
-});
-
-const store = createStore(rootReducer);
+import store from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
