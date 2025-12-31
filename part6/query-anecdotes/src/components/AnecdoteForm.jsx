aae8486ea5b13d0fe625e0f691d7e3c829aa19cf
Abel Osaretin <contact.abel321@gmail.com>
2025-12-31 13:33:16 +0100

    Exercise 6.21

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,9 +1,22 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { createAnecdote } from '../requests'
+
 const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+  const newAnecdoteMutation = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: (newAnecdote) => {
+      const notes = queryClient.getQueryData(['anecdotes'])
+      queryClient.setQueryData(['anecdotes'], notes.concat(newAnecdote))
+    },
+  })
+
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    console.log('new anecdote')
+    newAnecdoteMutation.mutate({ content, votes: 0 })
+    console.log(`new anecdote ${content}`)
   }
 
   return (
