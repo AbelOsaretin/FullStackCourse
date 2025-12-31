abecf472a022cb7eeed455adbc8762acad3a7779
Abel Osaretin <contact.abel321@gmail.com>
2025-12-31 09:52:07 +0100

    6.15 Anecdotes and the Backend, step 2

--- part6/redux-anecdotes/src/components/AnecdotesForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdotesForm.jsx
@@ -1,16 +1,18 @@
 import { useDispatch } from "react-redux";
 import { createAnecdote } from "../reducers/anecdoteReducer";
 import { showNotification } from "../reducers/notificationReducer";
+import anecdoteService from "../services/anecdotes";
 
 const AnecdotesForm = () => {
   const dispatch = useDispatch();
 
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault();
     const content = event.target.anecdoteTextField.value;
     event.target.anecdoteTextField.value = "";
-    dispatch(createAnecdote(content));
-    dispatch(showNotification(`anecdote '${content}' created`));
+    const newAnecdote = await anecdoteService.createNew(content);
+    dispatch(createAnecdote(newAnecdote));
+    dispatch(showNotification(`anecdote '${newAnecdote.content}' created`));
   };
 
   return (
